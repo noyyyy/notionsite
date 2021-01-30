d1b14955582d9654381c3110c91d56d3cfa2d3fc
Jerry <767729940@qq.com>
2021-01-30 18:15:08 +0800

    更改copyright和icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,7 +19,8 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2021 Travis Fischer</div>
+      <div className={styles.copyright}>Copyright 2021 Jerry Wang
+      </div>
 
       <div className={styles.settings}>
         <a
