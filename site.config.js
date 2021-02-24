a65f1fafd995b428cbf0b926b0fe257b16936068
Jerry <767729940@qq.com>
2021-02-24 16:25:32 +0800

    增加Pretty url 相关配置

--- site.config.js
+++ site.config.js
@@ -1,4 +1,10 @@
+const pageUrlOverrides = require('./page-overrides.json')
+  // top of site.config.js
+  // and then use pageUrlOverrides in your config
+
+
 module.exports = {
+
   // where it all starts -- the site's root Notion page (required)
   rootNotionPageId: '4e37b62ef87245369f859e4e1f7a2cab',
 
